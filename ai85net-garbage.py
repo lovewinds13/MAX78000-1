5567f797b11f57383c6dfcdd09d3b5f3a96f43ed
yuelong59 <88134407+yuelong59@users.noreply.github.com>
2022-11-26 13:19:12 +0800

    Add files via upload

--- ai85net-garbage.py
+++ ai85net-garbage.py
@@ -1,67 +1,201 @@
-import torch
-import torch.nn as nn
+# import torch
+# import torch.nn as nn
+
+# import ai8x
+
+# class AI85Net_GARBAGE(nn.Module):
+#     """
+#     5-Layer CNN that uses max parameters in AI84
+#     """
+#     def __init__(self, num_classes=6, num_channels=3, dimensions= (3, 224, 224),
+#                  planes=32, pool=2, fc_inputs=256, bias=False, **kwargs):
+#         super().__init__()
+
+#         # Limits
+#         assert planes + num_channels <= ai8x.dev.WEIGHT_INPUTS
+#         assert planes + fc_inputs <= ai8x.dev.WEIGHT_DEPTH-1
+
+#         # Keep track of image dimensions so one constructor works for all image sizes
+#         self.conv1 = ai8x.FusedConv2dBNReLU(num_channels, 16, 3, stride=1, padding=1, bias=True, **kwargs)
+#         self.conv2 = ai8x.FusedConv2dBNReLU(16, 32, 3, stride=2, padding=1, bias=True, **kwargs)
+#         self.conv3 = ai8x.FusedConv2dBNReLU(32, 64, 3, stride=2, padding=1, bias=True, **kwargs)
+#         self.conv4 = ai8x.FusedMaxPoolConv2d(64, 128, 3, pool_size=2, pool_stride=2, padding=1, bias=True, **kwargs)
+#         # self.conv4 = ai8x.FusedMaxPoolConv2dBNReLU(64, 128, 3, pool_size=2, pool_stride=2,
+#         #                                            stride=1, padding=1, bias=bias, **kwargs)
+
+#         self.pooling=ai8x.MaxPool2d(8)
+        
+#         self.avg = nn.AdaptiveAvgPool2d(1)#自适应平均池化
+        
+#         self.fc1 = ai8x.FusedLinearReLU(128, 128, bias=True, **kwargs)
+#         self.fc = ai8x.Linear(128, num_classes, bias=True, wide=True, **kwargs)
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+#     def forward(self, x):  # pylint: disable=arguments-differ
+#         """Forward prop"""
+#         x = self.conv1(x)
+#         x = self.conv2(x)
+#         x = self.conv3(x)
+#         x = self.conv4(x)
+#         # print(x.size())
+#         x = self.pooling(x)
+#         x = self.avg(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.fc1(x)
+#         x = self.fc(x)
+
+#         return x
+
+    
+# def ai85net_garbage(pretrained=False, **kwargs):
+#     """
+#     Constructs a AI85Net5 model.
+#     """
+#     assert not pretrained
+#     return AI85Net_GARBAGE(**kwargs)
+
+# models = [
+#     {
+#         'name': 'ai85net_garbage',
+#         'min_input': 1,
+#         'dim': 1,
+#     },
+
+# ]
+
+
+
+
+
+###################################################################################################
+#
+# Copyright (C) 2020 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+SimpleNet_v1 network with added residual layers for AI85.
+Simplified version of the network proposed in [1].
+
+[1] HasanPour, Seyyed Hossein, et al. "Lets keep it simple, using simple architectures to
+    outperform deeper and more complex architectures." arXiv preprint arXiv:1608.06037 (2016).
+"""
+from torch import nn
 
 import ai8x
 
-class AI85Net_GARBAGE(nn.Module):
+
+class AI85ResidualSimpleNet(nn.Module):
     """
-    5-Layer CNN that uses max parameters in AI84
+    Residual SimpleNet v1 Model
     """
-    def __init__(self, num_classes=6, num_channels=3, dimensions= (3, 224, 224),
-                 planes=32, pool=2, fc_inputs=256, bias=False, **kwargs):
+    def __init__(
+            self,
+#             num_classes=100,
+            num_classes=6,
+            num_channels=3,
+#             dimensions=(32, 32),  # pylint: disable=unused-argument
+            dimensions= (3, 224, 224),
+            bias=False,
+            **kwargs
+    ):
         super().__init__()
 
-        # Limits
-        assert planes + num_channels <= ai8x.dev.WEIGHT_INPUTS
-        assert planes + fc_inputs <= ai8x.dev.WEIGHT_DEPTH-1
-
-        # Keep track of image dimensions so one constructor works for all image sizes
-        self.conv1 = ai8x.FusedConv2dBNReLU(num_channels, 16, 3, stride=1, padding=1, bias=True, **kwargs)
-        self.conv2 = ai8x.FusedConv2dBNReLU(16, 32, 3, stride=2, padding=1, bias=True, **kwargs)
-        self.conv3 = ai8x.FusedConv2dBNReLU(32, 64, 3, stride=2, padding=1, bias=True, **kwargs)
-        self.conv4 = ai8x.FusedMaxPoolConv2d(64, 128, 3, pool_size=2, pool_stride=2, padding=1, bias=True, **kwargs)
-        # self.conv4 = ai8x.FusedMaxPoolConv2dBNReLU(64, 128, 3, pool_size=2, pool_stride=2,
-        #                                            stride=1, padding=1, bias=bias, **kwargs)
-
-        self.pooling=ai8x.MaxPool2d(8)
+        self.conv1 = ai8x.FusedConv2dReLU(num_channels, 16, 3, stride=1, padding=1, bias=bias,
+                                          **kwargs)
+        self.conv2 = ai8x.FusedConv2dReLU(16, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.conv3 = ai8x.FusedConv2dReLU(20, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.conv4 = ai8x.FusedConv2dReLU(20, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.resid1 = ai8x.Add()
+        self.conv5 = ai8x.FusedMaxPoolConv2dReLU(20, 20, 3, pool_size=2, pool_stride=2,
+                                                 stride=1, padding=1, bias=bias, **kwargs)
+        self.conv6 = ai8x.FusedConv2dReLU(20, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.resid2 = ai8x.Add()
+        self.conv7 = ai8x.FusedConv2dReLU(20, 44, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.conv8 = ai8x.FusedMaxPoolConv2dReLU(44, 48, 3, pool_size=2, pool_stride=2,
+                                                 stride=1, padding=1, bias=bias, **kwargs)
+        self.conv9 = ai8x.FusedConv2dReLU(48, 48, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.resid3 = ai8x.Add()
+        self.conv10 = ai8x.FusedMaxPoolConv2dReLU(48, 96, 3, pool_size=2, pool_stride=2,
+                                                  stride=1, padding=1, bias=bias, **kwargs)
+        self.conv11 = ai8x.FusedMaxPoolConv2dReLU(96, 512, 1, pool_size=2, pool_stride=2,
+                                                  padding=0, bias=bias, **kwargs)
+        self.conv12 = ai8x.FusedConv2dReLU(512, 128, 1, stride=1, padding=0, bias=bias, **kwargs)
+        self.conv13 = ai8x.FusedMaxPoolConv2dReLU(128, 128, 3, pool_size=2, pool_stride=2,
+                                                  stride=1, padding=1, bias=bias, **kwargs)
+        self.conv14 = ai8x.Conv2d(128, num_classes, 1, stride=1, padding=0, bias=bias,
+                                  wide=True, **kwargs)
         
         self.avg = nn.AdaptiveAvgPool2d(1)#自适应平均池化
-        
-        self.fc1 = ai8x.FusedLinearReLU(128, 128, bias=True, **kwargs)
+        self.fc1 = ai8x.FusedLinearReLU(num_classes, 128, bias=True, **kwargs)
         self.fc = ai8x.Linear(128, num_classes, bias=True, wide=True, **kwargs)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def forward(self, x):  # pylint: disable=arguments-differ
         """Forward prop"""
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        # print(x.size())
-        x = self.pooling(x)
+        x = self.conv1(x)          # 16x32x32
+        x_res = self.conv2(x)      # 20x32x32
+        x = self.conv3(x_res)      # 20x32x32
+        x = self.resid1(x, x_res)  # 20x32x32
+        x = self.conv4(x)          # 20x32x32
+        x_res = self.conv5(x)      # 20x16x16
+        x = self.conv6(x_res)      # 20x16x16
+        x = self.resid2(x, x_res)  # 20x16x16
+        x = self.conv7(x)          # 44x16x16
+        x_res = self.conv8(x)      # 48x8x8
+        x = self.conv9(x_res)      # 48x8x8
+        x = self.resid3(x, x_res)  # 48x8x8
+        x = self.conv10(x)         # 96x4x4
+        x = self.conv11(x)         # 512x2x2
+        x = self.conv12(x)         # 128x2x2
+        x = self.conv13(x)         # 128x1x1
+        x = self.conv14(x)         # num_classesx1x1
+        
         x = self.avg(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.fc(x)
-
         return x
 
-    
+
+# def ai85ressimplenet(pretrained=False, **kwargs):
+#     """
+#     Constructs a Residual SimpleNet v1 model.
+#     """
+#     assert not pretrained
+#     return AI85ResidualSimpleNet(**kwargs)
+
+
+# models = [
+#     {
+#         'name': 'ai85ressimplenet',
+#         'min_input': 1,
+#         'dim': 2,
+#     },
+# ]
+
+
+
+
+
 def ai85net_garbage(pretrained=False, **kwargs):
     """
     Constructs a AI85Net5 model.
     """
     assert not pretrained
-    return AI85Net_GARBAGE(**kwargs)
+    return AI85ResidualSimpleNet(**kwargs)
 
 models = [
     {
         'name': 'ai85net_garbage',
         'min_input': 1,
-        'dim': 1,
+        'dim': 2,
     },
 
 ]
+
+
